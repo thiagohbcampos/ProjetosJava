93ca9c6e57b369948e13668be1cd492f08ced83e
Thiago Henrique <thiagohbcampos@gmail.com>
2020-11-25 13:58:10 -0300

    Versao inicial

---
+++ DiasVividos/src/diasvividos/DiasVividos.java
@@ -0,0 +1,18 @@
+
+package diasvividos;
+
+/**
+ *
+ * @author Thiago Henrique
+ */
+public class DiasVividos {
+
+    public static void main(String[] args) {
+        int idade;
+        idade = 38;
+               
+      System.out.println("A pessoa com esta idade já viveu aproximadamente");
+      System.out.println(idade*365 + " Dias");
+    }
+    
+}
